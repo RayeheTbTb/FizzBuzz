c3b4fad2ce35826b9238c15ebb3c0470d7ed0977
Rayehe <rayehetabatabaei1986@gmail.com>
2022-04-24 11:02:31 +0430

    FizzBuzz project Logic & Test initialization

---
+++ FizzBuzz.Logic.Tests/FizzBuzzTest.cs
@@ -0,0 +1,20 @@
+﻿using FizzBuzzGameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz.Logic.Tests
+{
+    public class FizzBuzzTest
+    {
+        private readonly FizzBuzzGame _sut;
+
+        public FizzBuzzTest()
+        {
+            _sut = new FizzBuzzGame();
+        }
+    }
+}
