7364c4089e64cf48b907668e5350215cd4db25f9
Rayehe <rayehetabatabaei1986@gmail.com>
2022-04-24 11:35:52 +0430

    implemented FizzBuzzGame logic

--- FizzBuzzGameLogic/FizzBuzzGame.cs
+++ FizzBuzzGameLogic/FizzBuzzGame.cs
@@ -8,5 +8,24 @@ namespace FizzBuzzGameLogic
 {
     public class FizzBuzzGame
     {
+        public string CheckNumber(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                return "FizzBuzz";
+            }
+
+            return "No FizzBuzz";
+        }
     }
 }
